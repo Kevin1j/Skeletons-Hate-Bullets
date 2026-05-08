8ec74c21a3e9fa8b44bb0b53df9323e760cfc961
Kevin Johnson <johnsonrkevin@gmail.com>
2026-05-07 18:44:08 -0700

    got rid of some junk

--- scripts/enemy_scripts/enemy_spawner.gd
+++ scripts/enemy_scripts/enemy_spawner.gd
@@ -1,27 +1,40 @@
 extends Node2D
 
-@export var interval = .25 #seconds between enemy spawn
+var interval = .25 #seconds between enemy spawn
 @export var SwordSkeleton: PackedScene 
 @export var BigSkeleton: PackedScene
+var spawnables
+
 
 func _ready():
 	randomize()
 	spawn_loop()
 	
 func spawn_loop():
+	var random_pos = 0
+	var spawn
 	while true:
 		await get_tree().create_timer(interval).timeout
-		var random_pos = Vector2(
+		var spawn_chance = randi_range(0,99)
+		if spawn_chance < 5: #5% change to spawn big skeleton
+			spawn = BigSkeleton
+		elif spawn_chance < 0:
+			pass #change to give other skeletons a change to spawn
+		else:
+			spawn = SwordSkeleton
+		random_pos = Vector2(
 			randf_range(318, 4275),
 			randf_range(-2484, 1702))
-		spawn_enemy(random_pos, SwordSkeleton, 6.5)
+		spawn_enemy(spawn, random_pos)
 
 func _process(_delta):
-	pass
+	if Input.is_action_just_pressed("shoot"):
+		print("Mouse at: ", get_global_mouse_position())
 
-func spawn_enemy(position: Vector2, enemyType: PackedScene, scale: int):
-	var skelle = enemyType.instantiate()
-	skelle.position = position
-	skelle.scale = Vector2(scale, scale)
-	get_tree().current_scene.add_child(skelle)
+func spawn_enemy(bad_guy, pos):
+	#var screen_size = get_viewport_rect().size
+	var skeleton = bad_guy.instantiate()
+	skeleton.position = pos  
+	skeleton.scale = Vector2(6.5,6.5)
+	get_tree().current_scene.add_child(skeleton)
 		
