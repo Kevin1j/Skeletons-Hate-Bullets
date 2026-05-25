a75f0b9886e069a4f861cd2a7e6eb01fe58b12b2
Kevin Johnson <johnsonrkevin@gmail.com>
2026-05-25 16:22:31 -0700

    everything is done for v0.1.0git add .

--- scripts/Global/score_manager.gd
+++ scripts/Global/score_manager.gd
@@ -3,6 +3,7 @@ extends Node
 var score = 0
 var bones = 0
 var time = 0.00
+var highscore = 0
 
 var weapons_unlocked = {
 	"ak_47": true,
--- scripts/UI/restart_prompt.gd
+++ scripts/UI/restart_prompt.gd
@@ -2,20 +2,25 @@ extends Control
 
 @export var player_node: Node2D
 var score_label
+var high_score_label
 signal round_over
 
 func _ready():
 	visible = false
 	score_label = $Panel/VBoxContainer/Score
+	high_score_label = $Panel/VBoxContainer/HighScore
 	
 	#Define Signals
 	player_node.player_died.connect(_on_player_died)
 	
 func _on_player_died() -> void:
 	stop()
+	if ScoreManager.score > ScoreManager.highscore:
+		ScoreManager.highscore = ScoreManager.score
 	round_over.emit()
 	visible = true
 	score_label.text = "Score: " + str(ScoreManager.score)
+	high_score_label.text = "High Score: " + str(ScoreManager.highscore)
 
 func _on_restart_button_pressed() -> void:
 	await get_tree().process_frame
